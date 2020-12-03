f6053c7499d095ce50097cfc15dee53304d460a6
Hawk-55 <harshalfegade500@gmail.com>
2020-12-03 18:55:11 +0530

    Done

--- aud.py
+++ aud.py
@@ -8,5 +8,26 @@ print(audio)
 audio_slice = audio[100:]
 
 # To remove last dimension
-audio_tensor = tf.squeeze(audio_slice, axis=[-1])
+audio_tensor = tf.squeeze(audio_slice)
 print(audio_tensor)
+
+# To play the audio
+from pydub import AudioSegment
+from pydub.playback import play
+
+audio_clip = AudioSegment.from_mp3("/home/harshal/Music/TURKISH_FEMALE_BURCU.mp3")
+#play(audio_clip)
+
+
+# To draw graph of .mp3 file
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(audio_tensor.numpy())
+#plt.show()
+
+
+
+
+
+
